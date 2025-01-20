483b80f1d19c3a5133cc094c320e329c0ff47cc9
kai1992cool <kai1992cool@gmail.com>
2025-01-20 13:43:09 +0530

    dev

--- check-in/src/app/app.config.ts
+++ check-in/src/app/app.config.ts
@@ -5,10 +5,12 @@ import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimations(),
     provideHttpClient(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
